4d385243e8f3ea9765521d1436bcfb7d6b9040ac
SamSepiool <simone.severini@outlook.it>
2021-09-08 11:23:33 +0200

    snack-1 done

---
+++ snack-1/main.js
@@ -0,0 +1,70 @@
+// Crea un array di 10 oggetti che rappresentano una zucchina, indicando
+// per ognuna varietà, peso e lunghezza.
+// Calcola quanto pesano tutte le zucchine.
+
+// Crea un array di 10 oggetti che rappresentano una zucchina
+var zucchini = [
+    {
+        'varietà': 1,
+        'peso': 3,
+        'lunghezza': 24
+    },
+    {
+        'varietà': 2,
+        'peso': 1,
+        'lunghezza': 22
+    },
+    {
+        'varietà': 3,
+        'peso': 7,
+        'lunghezza': 21
+    },
+    {
+        'varietà': 4,
+        'peso': 9,
+        'lunghezza': 20
+    },
+    {
+        'varietà': 5,
+        'peso': 34,
+        'lunghezza': 29
+    },
+    {
+        'varietà': 6,
+        'peso': 12,
+        'lunghezza': 9
+    },
+    {
+        'varietà': 7,
+        'peso': 31,
+        'lunghezza': 7
+    },
+    {
+        'varietà': 8,
+        'peso': 35,
+        'lunghezza': 4
+    },
+    {
+        'varietà': 9,
+        'peso': 32,
+        'lunghezza': 2
+    },
+    {
+        'varietà': 10,
+        'peso': 7,
+        'lunghezza': 8
+    },
+
+]
+
+// Calcola quanto pesano tutte le zucchine.
+
+var zucchiniTotalWeight = 0;
+
+
+for (var i = 0; i < zucchini.length; i++) {
+    console.log('varietà: ' + zucchini[i]['peso']);
+    zucchiniTotalWeight += zucchini[i]['peso'];
+}
+
+alert('peso totale zucchine: ' + zucchiniTotalWeight);
